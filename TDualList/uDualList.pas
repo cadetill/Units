11efe237e359b3b17287315349717f5f612ce8e5
cadetill <cadetill@gmail.com>
2022-02-26 00:36:52 +0100

    .

--- TDualList/uDualList.pas
+++ TDualList/uDualList.pas
@@ -2,13 +2,14 @@
   @abstract(Unit to manage a dual list with Key-Value)
   @author(Xavier Martinez (cadetill) <cadetill@gmail.com>)
   @created(July 31, 2021)
-  @lastmod(July 31, 2021)
+  @lastmod(Febrary 31, 2021)
 
   The uDualList unit contains the definition and implementation of the classes needed to manage a dual list with Key-Value.
 
   @bold(Change List) @br
   @unorderedList(
     @item(31/07/2021 : first version)
+    @item(04/02/2022 : use generics for TDualList)
   )
 }
 unit uDualList;
@@ -44,152 +45,10 @@ type
     @abstract(Class that maintains a list of @link(TDualListItem).)
     Use a TDualList object to store and manipulate a list of a pair key-value.
   }
-  TDualList = class
-  private
-    FItems: TList<TDualListItem>;
-    function GetItem(Index: Integer): TDualListItem;
-    procedure SetItem(Index: Integer; const Value: TDualListItem);
-    function GetCount: Integer;
-  public
-    // constructor class
-    constructor Create;
-    // destructor class
-    destructor Destroy; override;
-
-    // Adds a new @link(TDualListItem).
-    function Add: TDualListItem; overload;
-    // Adds a new @link(TDualListItem) with the Key-Value values.
-    function Add(Key, Value: string): TDualListItem; overload;
-    // Inserts a new @link(TDualListItem) into Index position.
-    function Insert(Index: Integer): TDualListItem;
-    // Finds a @link(TDualListItem) into the list by a Key value and returns his Value.
-    function Find(Key: string): string;
-    // Finds a @link(TDualListItem) into the list by a Key value and returns true if exists or false if not.
-    function KeyExists(Key: string): Boolean;
-    // Finds a @link(TDualListItem) into the list by a Key value and returns his position.
-    function IndexOf(Key: string): Integer;
-    // Deletes a @link(TDualListItem) from the list by a Index.
-    procedure Delete(Index: Integer);
-    // Deletes all @link(TDualListItem) from the list.
-    procedure Clear;
-    // Copies the contents of another similar object.
-    procedure Assign(Source: TObject); virtual;
-
-    // List of all @link(TDualListItem).
-    property Items[Index: Integer]: TDualListItem read GetItem write SetItem;
-    // Indicate the number of @link(TDualListItem) in the list.
-    property Count: Integer read GetCount;
-  end;
+  TDualList = class(TList<TDualListItem>);
 
 implementation
 
-uses
-  System.SysUtils;
-
-{ TDualList }
-
-function TDualList.Add: TDualListItem;
-begin
-  Result := TDualListItem.Create;
-  FItems.Add(Result);
-end;
-
-function TDualList.Add(Key, Value: string): TDualListItem;
-begin
-  Result := TDualListItem.Create;
-  Result.Key := Key;
-  Result.Value := Value;
-  FItems.Add(Result);
-end;
-
-procedure TDualList.Assign(Source: TObject);
-var
-  i: Integer;
-begin
-  inherited;
-
-  if not (Source is TDualList) then
-    Exit;
-
-  Clear;
-  for i := 0 to TDualList(Source).Count - 1 do
-    Add(TDualList(Source).Items[i].Key, TDualList(Source).Items[i].Value);
-end;
-
-procedure TDualList.Clear;
-begin
-  FItems.Clear;
-end;
-
-constructor TDualList.Create;
-begin
-  FItems := TList<TDualListItem>.Create;
-end;
-
-procedure TDualList.Delete(Index: Integer);
-begin
-  FItems.Delete(Index);
-end;
-
-destructor TDualList.Destroy;
-begin
-  if Assigned(FItems) then
-  begin
-    Clear;
-    FItems.DisposeOf;
-  end;
-
-  inherited;
-end;
-
-function TDualList.Find(Key: string): string;
-var
-  i: Integer;
-begin
-  Result := '';
-  i := IndexOf(Key);
-  if i <> -1 then
-    Result := FItems[i].Value;
-end;
-
-function TDualList.GetCount: Integer;
-begin
-  Result := FItems.Count;
-end;
-
-function TDualList.GetItem(Index: Integer): TDualListItem;
-begin
-  Result := FItems[Index];
-end;
-
-function TDualList.IndexOf(Key: string): Integer;
-var
-  i: Integer;
-begin
-  Result := -1;
-  for i := 0 to GetCount - 1 do
-    if SameText(Key, FItems[i].Key) then
-    begin
-      Result := i;
-      Break;
-    end;
-end;
-
-function TDualList.Insert(Index: Integer): TDualListItem;
-begin
-  Result := TDualListItem.Create;
-  FItems.Insert(Index, Result);
-end;
-
-function TDualList.KeyExists(Key: string): Boolean;
-begin
-  Result := IndexOf(Key) <> -1;
-end;
-
-procedure TDualList.SetItem(Index: Integer; const Value: TDualListItem);
-begin
-  TDualListItem(FItems[Index]).Assign(Value);
-end;
 
 { TDualListItem }
 
